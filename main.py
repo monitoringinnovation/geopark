f9d0889ad698f1f694ea9c3e965711a00e6f5ae9
Dev-Alex-H <henaochacon@gmail.com>
2023-11-15 14:45:43 -0500

    add exec venv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/opt/geoparkCode/geopark/venv/bin/python3
-activate_this = '/ruta/completa/al/entorno_virtual/bin/activate'
+activate_this = '/opt/geoparkCode/geopark/venv/bin/activate'
 with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
 
