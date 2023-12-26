fb2bdce55ce2746d7e221ce6ad8aa2411344758c
Dev-Alex-H <henaochacon@gmail.com>
2023-12-26 16:58:37 -0500

    correct coordinates

--- main.py
+++ main.py
@@ -30,36 +30,56 @@ def getTokenGeo():
     global_token_geo = token_geo
     return token_geo
 
-def get_event(data_motion):
-    placa = data_motion["placa"]
-    motion_url = 'http://monitoringinnovation.com/api/geopark/create_event'
-    res_motion = requests.post(motion_url, data=data_motion)
-    logins = res_motion.json()
-    event_url = 'http://monitoringinnovation.com/api/geopark/get_last_event'
-    res_event = requests.post(event_url, data=placa)
-    logins_event = res_motion.json()
-    eventType = logins_event["event_type"]
-    return eventType
+def obtener_epoch_medianoche_actual():
+    fecha_actual = datetime.datetime.now().date()
+    medianoche = datetime.datetime.combine(fecha_actual, datetime.time.min)
+    epoch_medianoche = int(time.mktime(medianoche.timetuple()))
+    return epoch_medianoche + 18000
+
+def get_coordinates(id_unit, sid):
+    url_unload_msg = 'https://hst-api.wialon.com/wialon/ajax.html?svc=messages/unload&params={}&sid=' + sid
+    res_unload_msg = requests.get(url_unload_msg)
+    epoch_time_left = obtener_epoch_medianoche_actual()
+    epoch_time_right = epoch_time_left + 86399
+    url_coordinates = 'https://hst-api.wialon.com/wialon/ajax.html?svc=messages/load_interval&params={"itemId":'+ id_unit +',"timeFrom":' + epoch_time_left + ',"timeTo":' + epoch_time_right + ',"flags":1,"flagsMask":65281,"loadCount":1}&sid=' + sid
+    res_coordinates = requests.get(url_coordinates)
+    logins_coordinates = res_coordinates.json()
+    latitud = logins_coordinates["messages"][0]["pos"]["y"]
+    longitud = logins_coordinates["messages"][0]["pos"]["x"]
+    altitud = logins_coordinates["messages"][0]["pos"]["z"]
+    heading = logins_coordinates["messages"][0]["pos"]["c"]    
+    speed = logins_coordinates["messages"][0]["pos"]["s"]
+    utc_datetime = datetime.utcfromtimestamp(logins_coordinates["messages"][0]["t"]).strftime('%m/%d/%Y %H:%M:%S')
+    time_utc = datetime.strptime(utc_datetime, '%m/%d/%Y %H:%M:%S')
+    res = {
+    "latitud": latitud,
+    "longitud": longitud,
+    "altitud": altitud,
+    "heading": float(heading),
+    "time_utc": time_utc,
+    "speed": speed
+    }
+    return res
+
+# def get_event(data_motion):
+#     placa = data_motion["placa"]
+#     event_url = 'http://monitoringinnovation.com/api/geopark/get_last_event/placa=' + placa
+#     res_event = requests.get(event_url)
+#     logins_event = res_event.json()
+#     eventType = logins_event["result"]
+#     return eventType
+
+# def create_event_motion(data_motion):
+    data_to_send = {"params": data_motion}
+    event_url = 'http://monitoringinnovation.com/api/geopark/create_event'
+    res_event = requests.post(event_url, data=data_to_send)
+    logins_event = res_event.json()
+    return logins_event
 
 def transform_wialon_to_soap(wialon_data):
     global global_token_geo
     # Extract relevant information from Wialon data
-    packet_size = int(wialon_data[0:8], 16)
     controller_identifier = wialon_data[8:40]
-    utc_time = int(wialon_data[40:48], 16)
-    longitude = struct.unpack('<d', bytes.fromhex(wialon_data[88:104]))[0]
-    latitude = struct.unpack('<d', bytes.fromhex(wialon_data[104:120]))[0]
-    altitude = int(struct.unpack('<d', bytes.fromhex(wialon_data[120:136]))[0])
-    speed = int(wialon_data[136:140], 16)
-    course = int(wialon_data[140:144], 16)
-    satellites = int(wialon_data[136:138], 16)
-    power_ext_value = float.fromhex(wialon_data[142:158])
-    avl_inputs_value = int(wialon_data[160:162], 16)
-
-    # Convert UTC time to a readable format
-    
-    utc_datetime = datetime.utcfromtimestamp(utc_time).strftime('%m/%d/%Y %H:%M:%S')
-    time_utc = datetime.strptime(utc_datetime, '%m/%d/%Y %H:%M:%S')
 
     #Get wialon data
     sid = getSid()
@@ -67,11 +87,6 @@ def transform_wialon_to_soap(wialon_data):
     url_imei = 'https://hst-api.wialon.com/wialon/ajax.html?svc=core/search_items&params={"spec":{"itemsType":"avl_unit","propName":"sys_unique_id","propValueMask":"' + imei_unit + '","sortType":"sys_unique_id"},"force":1,"flags":1,"from":0,"to":0}&sid='+sid
     res_imei = requests.get(url_imei)
     logins_imei = res_imei.json()
-    print("ttttttttttttttttttt")
-    print("ttttttttttttttttttt")
-    print(logins_imei)
-    print("ttttttttttttttttttt")
-    print("ttttttttttttttttttt")
     id_unit = str(logins_imei["items"][0]["id"])
     url_data = 'https://hst-api.wialon.com/wialon/ajax.html?svc=core/search_item&params={%22id%22:' + id_unit + ',%22flags%22:1060865}&sid=' + sid
     res_data = requests.get(url_data)
@@ -82,16 +97,24 @@ def transform_wialon_to_soap(wialon_data):
             ignition_key = key.get("p")
     prms_vals = logins["item"]["prms"]
     ignition_value = prms_vals.get(ignition_key)
-    eventCode = get_event(ignition_value)
     odometer = logins["item"]["cnm"]
     placa = logins["item"]["nm"]
+    data_coordinates = get_coordinates(id_unit, sid)
+    time_utc = data_coordinates["time_utc"]
+    latitude = data_coordinates["latitud"]
+    longitude = data_coordinates["longitud"]
+    altitude = data_coordinates["altitud"]
+    course = data_coordinates["heading"]
+    speed = data_coordinates["speed"]
 
-    print(global_token_geo)
+    # eventCode = get_event(placa)
+    # event_type_f = eventCode if eventCode != "00" else "01"
+    event_type_f = "01"
 
     # Create SOAP request payload
     payload = {
         'placa': placa,
-        'eventTypeCode': eventCode,
+        'eventTypeCode': event_type_f,
         'dateTimeUTC': time_utc,
         'GPSStatus': True,
         'latitude': latitude,
@@ -103,10 +126,11 @@ def transform_wialon_to_soap(wialon_data):
         'engineStatus': True if ignition_value == 1 else False,
         'userToken': global_token_geo,
     }
-    eventCode = get_event(payload)
-
+    print("payload")
+    print("payload")
     print(payload)
-
+    print("payload")
+    print("payload")
     return payload
 
 def send_soap_request(payload):
@@ -121,14 +145,7 @@ def send_soap_request(payload):
 
 def handle_client(client_socket):
     request_data = client_socket.recv(1024)  # Adjust buffer size as needed
-    wialon_data = request_data.hex()
-
-    print("wiadata $$$$")
-    print("wiadata $$$$")
-    print(wialon_data)
-    print("wiadata $$$$")
-    print("wiadata $$$$")
-    
+    wialon_data = request_data.hex()    
     # Transform Wialon data to SOAP request format
     soap_payload = transform_wialon_to_soap(wialon_data)
 
